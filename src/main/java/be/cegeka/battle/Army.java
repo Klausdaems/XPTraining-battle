e720a70eb38c22b8f039c0c2423f99211248c3e5
Programmer <Lindert_vdp@hotmail.com>
2018-05-24 12:01:18 +0200

    US4
    I can enroll soldiers in an army.
    The front man of an army is the first enlisted soldier.

---
+++ src/main/java/be/cegeka/battle/Army.java
@@ -0,0 +1,49 @@
+package be.cegeka.battle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+
+    private List<Soldier> soldiers = new ArrayList<>();
+
+    private Soldier soldier;
+
+    private Soldier frontMan;
+
+    public Army() {
+
+    }
+
+    public List<Soldier> getSoldiers() {
+        return soldiers;
+    }
+
+    public void setSoldiers(List<Soldier> soldiers) {
+        this.soldiers = soldiers;
+    }
+
+    public Soldier getSoldier() {
+        return soldier;
+    }
+
+    public void setSoldier(Soldier soldier) {
+        this.soldier = soldier;
+    }
+
+    public Soldier getFrontMan() {
+        return frontMan;
+    }
+
+
+    public void setFrontMan(Soldier frontMan) {
+        this.frontMan = frontMan;
+    }
+
+
+    public void enrollSoldier(Soldier soldier) {
+        this.soldier = soldier;
+        soldiers.add(soldier);
+        this.frontMan = soldiers.get(0);
+    }
+}
---
+++ src/test/java/be/cegeka/battle/ArmyTest.java
@@ -0,0 +1,73 @@
+/*
+ * package be.cegeka.battle; import static org.assertj.core.api.Assertions.assertThat; import org.junit.Rule; import
+ * org.junit.Test; import org.junit.rules.ExpectedException; public class SoldierTest {
+ * @Rule public ExpectedException expectedException = ExpectedException.none();
+ * @Test public void construction_ASoldierMustHaveAName() { Soldier soldier = new Soldier("name");
+ * assertThat(soldier.getName()).isEqualTo("name"); }
+ * @Test public void construction_ASoldierMustHaveAName_CannotBeNull() {
+ * expectedException.expect(RuntimeException.class); expectedException.expectMessage("Name mag niet null zijn"); new
+ * Soldier(null); }
+ * @Test() public void construction_ASoldierMustHaveAName_CannotBeEmpty() {
+ * expectedException.expect(RuntimeException.class); expectedException.expectMessage("Name mag niet leeg zijn"); new
+ * Soldier(""); }
+ * @Test() public void construction_ASoldierMustHaveAName_CannotBeBlank() {
+ * expectedException.expect(RuntimeException.class); expectedException.expectMessage("Name mag geen spaties hebben");
+ * new Soldier("   "); } }
+ */
+package be.cegeka.battle;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ArmyTest {
+
+    public ArmyTest() {
+
+    }
+
+    private Army army;
+
+    private Soldier soldier1;
+
+    private Soldier soldier2;
+
+    private Soldier soldier3;
+
+    @Before
+    public void maakArmyAan() {
+        army = new Army();
+    }
+
+    @Before
+    public void maakSoldiersAanEnEnoll() {
+        soldier1 = new Soldier("Klied");
+        soldier2 = new Soldier("Lindert");
+        soldier3 = new Soldier("Stef");
+        army.enrollSoldier(soldier1);
+        army.enrollSoldier(soldier2);
+        army.enrollSoldier(soldier3);
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testBekijkOfIedereenSuccesvolToegevoegdIs() {
+        Assertions.assertThat(army.getSoldiers().size() == 3);
+    }
+
+    @Test
+    public void testDatKliedFrontmanIs() {
+        Assertions.assertThat(army.getSoldiers().get(0).equals(soldier1));
+    }
+
+    @Test
+    public void testDatKliedGeenFrontmanIs() {
+        Assertions.assertThat(army.getSoldiers().get(0)).isNotEqualTo(soldier2);
+    }
+
+
+}
