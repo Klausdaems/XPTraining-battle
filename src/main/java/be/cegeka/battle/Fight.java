76d10a81b5384c0054bbc1e6ff486e5029cd73d7
Emma <Lindert_vdp@hotmail.com>
2018-05-24 13:44:12 +0200

    Merge Try US6met7

--- src/main/java/be/cegeka/battle/Fight.java
+++ src/main/java/be/cegeka/battle/Fight.java
@@ -14,19 +14,17 @@ public class Fight {
     public Soldier fight(Soldier attacker, Soldier defender) {
         Soldier winner = null;
 
-        if (attacker.getWeapon().getDamagePoints() > defender.getWeapon().getDamagePoints()) {
+        if (attacker.getWeapon().getDamagePoints() >= defender.getWeapon().getDamagePoints()) {
             winner = attacker;
         }
 
         if (attacker.getWeapon().getDamagePoints() < defender.getWeapon().getDamagePoints()) {
             winner = defender;
         }
+
+
         Headquarters h = new Headquarters();
         h.ReportCasualty(winner.getId());
-
-        if (attacker.getWeapon().getDamagePoints() == defender.getWeapon().getDamagePoints()) {
-            winner = attacker;
-        }
 
         return winner;
     }
